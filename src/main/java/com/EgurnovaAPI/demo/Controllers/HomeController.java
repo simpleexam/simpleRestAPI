19679e5c8a800ca9c01802cbc262b4ff68f5ccad
Egurnova Elena <120087135+monomarine@users.noreply.github.com>
2024-04-17 09:28:51 +0500

    lesson3_end
    создан endpoint rest/home

---
+++ src/main/java/com/EgurnovaAPI/demo/Controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.EgurnovaAPI.demo.Controllers;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import  org.springframework.web.bind.annotation.RequestMethod;
+import  org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller@RequestMapping("/rest/home")
+public class HomeController {
+    @ResponseBody
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String hello(){
+        return "ответ с endpoint rest/home";
+    }
+}
